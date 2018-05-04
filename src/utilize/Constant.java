e5336b70c5d8a00b4cbf8bb74cb5e93ea75e3e2e
lorence <lorence@enclave.vn>
2018-05-04 17:35:24 +0700

    Add tool

---
+++ src/utilize/Constant.java
@@ -0,0 +1,59 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package utilize;
+
+/**
+ *
+ * @author lanch
+ */
+public class Constant {
+    
+    public static String TITLE = "AVTool-AndroidStudio";
+    
+    public static class MenuBar {
+        public static String FILE = "File";
+    }
+    
+    // FILE
+    public static class FILE {
+        public static String NEW_FILE = "Tạo file mới";
+        public static String OPEN_FILE = "Mở file";
+        public static String SAVE_FILE = "Lưu file";
+        public static String SAVE_AS_FILE = "Lưu sang file khác";
+        public static String IMPORT_OLD_VERSION = "Nhập dữ liệu từ phiên bản cũ";
+        public static String EXPORT_EXCEL = "Xuất file excel";
+        public static String EXIST = "Thoát";
+        public static String EXIS;
+    }
+    
+    // TOOL
+    public static class TOOL {
+        public static String OPEN_FOLDER = "Mở thư mục chứa mẫu bảng tính";
+    }
+    
+    // CALCULATOR
+    public static class CALCULATOR {
+        public static String CALCULATOR = "Tính toán";
+    }
+    
+    // HELP
+    public static class HELP {
+        public static String GUID = "Hướng dẫn sử dụng";
+        public static String KNOWLEDGE = "Kiến thức sử dụng phần mềm";
+        public static String ABOUT = "Giới thiệu";
+    }
+    
+    // Toolbar
+    public static String EMPTY_STRING = "";
+    
+   // Image
+    public static String UTILIZE_IMAGE = "/utilize/image/logo.png";
+    
+    // Tab
+    public static class TAB {
+        public static String GRADLE ="Gradle";
+    }
+}
